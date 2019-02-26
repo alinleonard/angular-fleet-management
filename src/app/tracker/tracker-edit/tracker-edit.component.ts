619159430615c3af92284086f29767a8ba43c423
Alin <alin.busila@cerner.com>
2019-02-26 16:53:49 +0200

    work on edit tracker

--- src/app/tracker/tracker-edit/tracker-edit.component.ts
+++ src/app/tracker/tracker-edit/tracker-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Vehicle } from 'src/app/vehicle/vehicle.model';
+import { VehicleService } from 'src/app/vehicle/vehicle.service';
 
 @Component({
   selector: 'app-tracker-edit',
@@ -12,21 +14,30 @@ export class TrackerEditComponent implements OnInit {
   isEdit = false;
 
   form: FormGroup;
+  vehicles: Vehicle[];
+  @ViewChild('selectedOption') selectedOptionRef: ElementRef;
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router,
+    private vehicleService: VehicleService) { }
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.trackerId = +params['id'];
       this.isEdit = params['id'] != null;
+      this.vehicles = this.vehicleService.getVehicles();
       this.initForm();
     });
   }
 
   initForm() {
     this.form = new FormGroup({
-
+      sn: new FormControl(null, Validators.required),
+      phone: new FormControl(null, Validators.required)
     });
+  }
+
+  onSetVehicle() {
+    const assignedVehicleId = this.selectedOptionRef.nativeElement.selectedOptions[0].value;
   }
 
   onSubmit() {
--- src/app/tracker/tracker-list/tracker-list.component.ts
+++ src/app/tracker/tracker-list/tracker-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Tracker } from '../tracker.model';
 import { TrackerService } from '../tracker.service';
 import { Subscription } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tracker-list',
@@ -12,13 +13,17 @@ export class TrackerListComponent implements OnInit, OnDestroy {
   trackers: Tracker[];
   private subscription: Subscription;
 
-  constructor(private trackerService: TrackerService) { }
+  constructor(private trackerService: TrackerService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.trackers = this.trackerService.getTrackers();
     this.subscription = this.trackerService.trackerChanged.subscribe((trackers) => {
       this.trackers = trackers;
     });
+  }
+
+  onNewTracker() {
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 
   ngOnDestroy() {
